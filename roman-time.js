1775b73cf4863d7963a9679448ddd9fb21dabfa2
natamaki <natamaki@yandex.ru>
2016-01-13 10:00:30 +0300

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,72 +1,29 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+var hours = Number(process.argv[2]);
+var minutes = Number(process.argv[3]);
 
-if (Number(hours) === 0 || Number(minutes) === 0) {
-    console.log("Нуля среди римских цифр нет");
-    return;
+var numbers = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
+                10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L' };
+
+if ((hours !== 0 && !hours) || (minutes !== 0 && !minutes)) {
+    throw new Error('Время нужно указать числами');
 }
 
-if (!Number(hours) || !Number(minutes)) {
-    console.log("Время необходимо указать числами");
-    return;
+if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59  ) {
+    throw new Error('Время указано не верно');
 }
 
-if (hours > 23 || minutes > 59) {
-    console.log("Время указано не верно");
-    return;
-}
-
-var numbers = {
-    1: 'I',
-    5: 'V',
-    10: 'X',
-    50: 'L'
-};
-
-function getRomanNumber (time) {
-    var tens = Math.floor(time/10); //получение целого числа десятков
-    var units = time - tens*10; //получение целого числа единиц
-
-    var tensArabical = '';
-    if (tens) {
-        if (tens >= 1 && tens <= 3) {
-            tensArabical = concatSameSymbols(numbers[10], tens);
-        } else if (tens === 4) {
-            tensArabical = numbers[10] + numbers[50]
-        } else {
-            tensArabical = numbers[50]
-        }
+function getRomanNumbers (time) {
+    if (time === 0) {
+        return '-';
     }
 
-    var unitsArabical = '';
-    if (units) {
-        if (units >= 1 && units <= 3) {
-            unitsArabical = concatSameSymbols(numbers[1], units);
-        } else if (units >= 4 && units <= 8) {
-            var val = units % 5;
+    var tensCalc = Math.floor(time/10)*10;
+    var tensRoman = tensCalc ? numbers[tensCalc] : '';
 
-            if (val >= 1 && val < 4) {
-                unitsArabical = numbers[5] + concatSameSymbols(numbers[1], val);
-            } else if (val === 0) {
-                unitsArabical = numbers[5];
-            } else {
-                unitsArabical = numbers[1] + numbers[5];
-            }
-        } else if (units === 9) {
-            unitsArabical = numbers[1] + numbers[10];
-        }
-    }
+    var unitsCalc = time - tensCalc;
+    var unitsRoman = unitsCalc ? numbers[unitsCalc] : '';
 
-    return tensArabical + unitsArabical;
+    return tensRoman + unitsRoman;
 }
 
-function concatSameSymbols (symbol, amount) {
-    var str = '';
-    for (i = 1; i <= amount; i++) {
-        str = str + symbol;
-    }
-    return str;
-}
-
-var result = getRomanNumber(hours) + ':' + getRomanNumber(minutes);
-console.log(result);
+console.log(getRomanNumbers(hours) + ':' + getRomanNumbers(minutes));
