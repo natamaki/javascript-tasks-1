81db6d0aea39100e5eb0f05d6a607fc1c7f2c001
natamaki <natamaki@yandex.ru>
2016-01-15 09:26:37 +0300

    fix

--- roman-time.js
+++ roman-time.js
@@ -1,15 +1,29 @@
 var hours = Number(process.argv[2]);
 var minutes = Number(process.argv[3]);
 
-var numbers = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX',
-                10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L' };
+var numbers = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L'
+};
 
-if ((hours !== 0 && !hours) || (minutes !== 0 && !minutes)) {
+if (isNaN(hours) || isNaN(minutes)) {
     throw new Error('Время нужно указать числами');
 }
 
 if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59  ) {
-    throw new Error('Время указано не верно');
+    throw new Error('Время указано неверно');
 }
 
 function getRomanNumbers (time) {
